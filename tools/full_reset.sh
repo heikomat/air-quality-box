aaa3861476f1628e5c7f8298d78c90023aff2cfa
Heiko Mathes <heiko@pop-os.localdomain>
2020-02-23 13:05:11 +0100

    add delay before uploading files for full-reset

--- tools/full_reset.sh
+++ tools/full_reset.sh
@@ -2,4 +2,5 @@
 ./tools/build_lfs.sh
 ./tools/flash_firmware_and_configure_lfs_partition.sh
 ./tools/flash_lfs.sh
-nodemcu-tool upload icons_bin/*
+sleep 8
+nodemcu-tool upload icons_bin/* src/init.lua src/_init.lua
